0f4a6f5a7f87cc77ba1dd9248b98712781c12310
sarumathi <110803703+saru039@users.noreply.github.com>
2023-03-06 20:41:40 +0000

    initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reducer, { initialState } from "./components/ContextApi/reducer";
+import { StateProvider } from "./components/ContextApi/StateProvider";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
